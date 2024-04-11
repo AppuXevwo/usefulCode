2697df7b870811cbb354c9e2c085cb3fadb5b210
AppuXevwo <160410986+AppuXevwo@users.noreply.github.com>
2024-04-11 17:03:51 +0530

    Update comparefile.py

--- comparefile.py
+++ comparefile.py
@@ -1,46 +1,56 @@
 import pandas as pd
 
-#Enter you file path
-path = ''
-#File to compare
-CompFile1 = f'{path}/employees1.csv'
-#File to compare with
-CompFile2 = f'{path}/employees.csv'
-#key columns to compare using
-CommonColumn = ['emp_no']
-# Read the two files into pandas DataFrames with file seperator as '|
-dfFile1 = pd.read_csv(CompFile1, sep='|')
-dfFile2 = pd.read_csv(CompFile2, sep='|')
 
-listheader1 = list(dfFile1.columns.values)
-listheader2 = list(dfFile2.columns.values)
-listheader = []
-if listheader2 == listheader1:
-    for val in listheader1:
-        listheader.append(f'{val}_file1')
-        listheader.append(f'{val}_file2')
-    # Merge the DataFrames to find common elements
-    common_df = pd.merge(dfFile1, dfFile2, how='inner', on='emp_no')
+def common_records(df1, df2, path, keys):
+    #common recrods
+    common_df = pd.merge(df1, df2, how='inner', on=keys)
+    common_df.to_csv(path + '/' + 'common_elements.csv', index=False)
 
-    # Find differences
 
-    # Get common key values
-    common_keys = set(dfFile1['emp_no']).intersection(dfFile2['emp_no'])
-
-    # Create an empty DataFrame to store the differences
+def difference_records(df1, df2, path, keys, header):
+    merge_df = pd.merge(df1, df2, how='outer', on=keys, indicator=True)
+    #records only in left file
+    left_df = merge_df[merge_df['_merge'] == 'left_only'].drop(columns='_merge')
+    left_df.to_csv(path + '/' + 'left_only.csv', index=False)
+    # records only in right file
+    right_df = merge_df[merge_df['_merge'] == 'right_only'].drop(columns='_merge')
+    right_df.to_csv(path + '/' + 'right_only.csv', index=False)
+    #common records
+    common_keys = set(df1[keys]).intersection(df2[keys])
     diff_df = pd.DataFrame()
-
     # Iterate over common key values and compare corresponding rows
     for key in common_keys:
-        df1_row = dfFile1[dfFile1['emp_no'] == key].iloc[0]
-        df2_row = dfFile2[dfFile2['emp_no'] == key].iloc[0]
+        df1_row = df1[df1[keys] == key].iloc[0]
+        df2_row = df2[df2[keys] == key].iloc[0]
         if not df1_row.equals(df2_row):
-            diff_row = {col+'_file1': df1_row[col] for col in dfFile1.columns}
-            diff_row.update({col+'_file2': df2_row[col] for col in dfFile2.columns})
+            diff_row = {col + '_file1': df1_row[col] for col in df1.columns}
+            diff_row.update({col + '_file2': df2_row[col] for col in df2.columns})
             diff_row = {k: [v] for k, v in diff_row.items()}
             diff_df = pd.concat([diff_df, pd.DataFrame(diff_row)], ignore_index=True)
+            diff_df = diff_df[header]
+            diff_df.to_csv(path + '/' + 'differences.csv', index=False)
 
 
-    diff_df = diff_df[listheader]
-    diff_df.to_csv(path + '/' +'differences.csv', index=False)
-    common_df.to_csv(path + '/' + 'common_elements.csv', index=False)
+#Enter file path
+path = 'G://Python/files/'
+#File to compare
+CompLeft = f'{path}/employees1.csv'
+#File to compare with
+CompRight = f'{path}/employees.csv'
+#key columns to compare using
+KeyColumn = ['emp_no']
+CommonCol = ','.join(KeyColumn)
+# Read the two files into pandas DataFrames with file seperator as '|
+dfLeft = pd.read_csv(CompLeft, sep='|')
+dfRight = pd.read_csv(CompRight, sep='|')
+
+headerLeft = dfRight.columns.to_list()
+headerRight = dfRight.columns.to_list()
+FinalHeader = []
+if headerLeft == headerRight:
+    for val in headerRight:
+        FinalHeader.append(f'{val.strip()}_file1')
+        FinalHeader.append(f'{val.strip()}_file2')
+    common_records(dfLeft, dfRight, path, CommonCol)
+    difference_records(dfLeft, dfRight, path, CommonCol, FinalHeader)
+
